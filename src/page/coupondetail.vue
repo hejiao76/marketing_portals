e903655117a727e8464f7cf6d3f0f1d3edde843b
vipkid <xingdaiyong@vipkid.com.cn>
2017-12-17 19:43:18 +0800

    merge

--- src/page/coupondetail.vue
+++ src/page/coupondetail.vue
@@ -1,50 +1,51 @@
 <template>
-  	<div>
-      <loading v-show="loadingShow"></loading>
-      <div class="center_box detailbox">
-        <!--领取状态-->
-        <div class="getist">
-          <div class="getBox getSucceed">
-            <div class="gettop">
-              <div>{{detail.name}}</div>
+  <div>
+    <loading v-show="loadingShow"></loading>
+    <div class="center_box detailbox">
+      <!--领取状态-->
+      <div class="getist">
+        <div class="getBox getSucceed">
+          <div class="gettop">
+            <div>{{detail.name}}</div>
+          </div>
+          <div class="detail_code">
+            <div class="amount"><span>￥</span>{{detail.amount}}</div>
+            <img class="codeimg" :src="imgurl"  />
+          </div>
+          <div class="centerlist">
+            <div>
+              <span class="spa">核销码</span>
+              <span class="spb codee">{{detail.couponCode}}</span>
+              <span class="statustype">{{Final.COUPON_STATUS[detail.status]}}</span>
             </div>
-            <div class="detail_code">
-              <div class="amount"><span>￥</span>{{detail.amount}}</div>
-              <img class="codeimg" :src="imgurl"  />
+            <div>
+              <span class="spa">可用的购车人</span>
+              <span class="spb">{{detail.userName}}</span>
             </div>
-            <div class="centerlist">
-              <div>
-                <span class="spa">核销码</span>
-                <span class="spb codee">{{detail.couponCode}}</span>
-                <span class="statustype">{{Final.COUPON_STATUS[detail.status]}}</span>
-              </div>
-              <div>
-                <span class="spa">可用的购车人</span>
-                <span class="spb">{{detail.userName}}</span>
-              </div>
-              <div>
-                <span class="spa">适用范围</span>
-                <span class="spb">{{detail.serialName}}</span>
-              </div>
-              <div class="">
-                <span class="spa">有效期</span>
-                <span class="spb">{{detail.validity.split(" ")[0]}}日前使用有效</span>
-              </div>
+            <div>
+              <span class="spa">适用范围</span>
+              <span class="spb">{{detail.serialName}}</span>
+            </div>
+            <div class="">
+              <span class="spa">有效期</span>
+              <span class="spb">{{detail.validity.split(" ")[0]}}日前使用有效</span>
+            </div>
 
-            </div>
-            <div class="centerlist qy_list">
-              <span class="spa">使用细则</span>
-              <ul class="qy_ul">
-                <li :key="index" v-for="(item,index,key) in  getsplit(detail.description)" v-if="index<4" >{{item}}</li>
-              </ul>
+          </div>
+          <div class="centerlist qy_list">
+            <span class="spa">使用细则</span>
+            <div class="qy_ul" v-html="detail.details">
+              <!--<li :key="index" v-for="(item,index,key) in  getsplit(detail.description)" v-if="index<4" >{{item}}</li>-->
+              <!---->
             </div>
           </div>
-
         </div>
 
       </div>
 
     </div>
+
+  </div>
 </template>
 <script>
   import api from "./../fetch/api"
@@ -52,60 +53,60 @@
   import API from "./../fetch/api";
   import loading from "../components/loading";
   export default {
-      data() {
-          return {
-              Final:Final,
-              imgurl:'',
-              loadingShow:false,
-              resDetails:'',
-            activityId:'',
-              detail:{titlename:'抵扣卷名称',status:1,amount:'888',codeimg:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3256036687,742666259&fm=27&gp=0.jpg',code:'ewwewewe',text:'祥云全系',name:'张三',timeout:'2019.08.23',qylist:['3000元保险增值礼包\n','3000元线上购车专享礼金\n', '2000元贴膜\n','2000元新车大礼包\n']}
-          }
-      },
-      mounted (){
-          this.activityId = this.$route.params.code;
-          this.imgurl=Final.QRCODE+"/v1/gift/qrcode?activityId="+this.activityId+"&activityType=103&couponCode=yunyong"
-          this.getmyCouponInfo({couponCode:this.activityId})
-      },
-      created (){
-        //alert("create");
-      },
-      components :{
-        loading
-      },
-      methods : {
-        /**
-         * 获取我的抵扣券列表
-         * @param params
-         * @returns {*}
-         */
-        getmyCouponInfo(data){
-          api.ap_my_coupon_info(data)
-           .then(res => {
-              if(res.status){
-                this.detail = res.result;
-              }
+    data() {
+      return {
+        Final:Final,
+        imgurl:'',
+        loadingShow:false,
+        resDetails:'',
+        activityId:'',
+        detail:{titlename:'抵扣卷名称',status:1,amount:'888',codeimg:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3256036687,742666259&fm=27&gp=0.jpg',code:'ewwewewe',text:'祥云全系',name:'张三',timeout:'2019.08.23',qylist:['3000元保险增值礼包\n','3000元线上购车专享礼金\n', '2000元贴膜\n','2000元新车大礼包\n']}
+      }
+    },
+    mounted (){
+      this.activityId = this.$route.params.code;
+      this.imgurl=Final.QRCODE+"/v1/gift/qrcode?activityId="+this.activityId+"&activityType=103&couponCode=yunyong"
+      this.getmyCouponInfo({couponCode:this.activityId})
+    },
+    created (){
+      //alert("create");
+    },
+    components :{
+      loading
+    },
+    methods : {
+      /**
+       * 获取我的抵扣券列表
+       * @param params
+       * @returns {*}
+       */
+      getmyCouponInfo(data){
+        api.ap_my_coupon_info(data)
+          .then(res => {
+            if(res.status){
+              this.detail = res.result;
+            }
           }).catch(error => {
 
-          })
-        },
-        Pickclick : function (data){
-          if(data.isGet==2){
-            return false;
-          }
-           // this.loadingShow=true;
-          let mobile=localStorage.mobile;
-          let realName=localStorage.realName;
-          if(mobile&&realName){
-            $(".login .username").val(realName);
-            $(".login .mobile").val(mobile);
+        })
+      },
+      Pickclick : function (data){
+        if(data.isGet==2){
+          return false;
+        }
+        // this.loadingShow=true;
+        let mobile=localStorage.mobile;
+        let realName=localStorage.realName;
+        if(mobile&&realName){
+          $(".login .username").val(realName);
+          $(".login .mobile").val(mobile);
 
-          }
-        },
-        getsplit:function(detail){
-            return detail.split("\n");
         }
       },
+      getsplit:function(detail){
+        return detail.split("\n");
+      }
+    },
   }
 
 </script>
@@ -116,7 +117,8 @@
   .detail_code { text-align: center; border-bottom: 1px solid #f1f1f1; padding-bottom: .15rem;}
   .detail_code .amount { font-size: .3rem; margin: .1rem auto .05rem;}
   .detail_code .amount>span { font-size: .18rem;}
-  .detail_code>img { width:1.3rem;}
+  .detail_code>img { width:1.3rem;
+    height:1.3rem;}
   .detailbox .getBox { padding-bottom: .2rem;}
   .detailbox .qy_list { border-bottom: none;}
   .detailbox .spa { color:#666;}
