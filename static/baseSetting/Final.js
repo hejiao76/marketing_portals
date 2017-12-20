a30cfe14810bf39b7b9b9c72d946607e1a5ed0c7
jsb-cpyy <hejiao@zhidianlife.com>
2017-12-20 16:12:02 +0800

    营销活动---C端系统

--- static/baseSetting/Final.js
+++ static/baseSetting/Final.js
@@ -8,8 +8,7 @@ const OBJ ={
   IMG_PATH:"http://img.familyku.com",  //图片访问地址配置
   QRCODE:'http://openapi.dev.yunyang.work', //核销码访问地址配置
 
-
-    SERIALTYPE:{
+  SERIALTYPE:{
     1:"全车系", 2:"指定车系"
   },
 
@@ -18,17 +17,6 @@ const OBJ ={
   },
   COUPON_STATUS:{
     0:"未使用", 1:"已使用" ,2:"快到期", 3:"已过期"
-  },
-
-  PATH_MAPPING : ["startWorkDetail","companyDetail","personalDetail","personalDetailInfo","driverDetail","driverDetailInfo","truckDriverDetail","truckDriverDetailInfo","truckDetail","truckDetailInfo"],  //左侧菜单适配路由名称
-  PATH_MAPPING_OBJ : {//左侧菜单适配路由映射地址
-    "startWorkDetail" : {name :"startWorkDetail",defaultPathMap:"/startwork/startwork_1"},
-    "companyDetail" : {name :"companyDetail",defaultPathMap:"/lgtcheck/companycheck/1"},
-    "personalDetail" : {name :"personalDetail",defaultPathMap:"/lgtcheck/personalcheck/0"},
-    "personalDetailInfo" : {name :"personalDetailInfo",defaultPathMap:"/lgtcheck/personalcheck/0"},
-    "driverDetail" : {name :"driverDetail",defaultPathMap:"/lgtcheck/drivercheck/0"},
-    "driverDetailInfo" : {name :"driverDetailInfo",defaultPathMap:"/lgtcheck/drivercheck/0"},
-    "truckDriverDetail" : {name :"truckDriverDetail",defaultPathMap:"/lgtcheck/truckDrivercheck/0"},
   }
 }
 export  default OBJ;
