91e3bb8ee350c65436daeafa15bcdac3729c9dbf
jsb-cpyy <hejiao@zhidianlife.com>
2017-12-26 03:50:21 +0800

    营销活动---C端系统  bug 修复

---
+++ src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <span @click="back"><<返回</span>
+      <span>{{title}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    props: ['title'],
+    mounted() {
+
+    },
+    methods : {
+        initPage (){
+
+        },
+        back () {
+            this.$router.go(-1);
+        }
+    },
+  }
+</script>
+<style>
+
+</style>
+
