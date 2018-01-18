24e3b2eecdcc5917661d03a5a5e821a457611977
jsb-cpyy <hejiao@zhidianlife.com>
2018-01-18 15:21:47 +0800

    营销活动---C端系统  bug 修复

--- static/baseSetting/Final.js
+++ static/baseSetting/Final.js
@@ -2,14 +2,14 @@
  * Created by jsb-cpyy on 2017/8/30.
  */
 const proxy='/action' //代理请求目标声明
-
+const payDomain = "http://static.EC_WEB_DOMAIN_REPLACE";
 const OBJ ={
   PROXY:proxy, //代理请求目标
   IMG_PATH:"http://img.familyku.com",  //图片访问地址配置
   QRCODE:'http://openapi.dev.yunyang.work', //核销码访问地址配置
   CHILD_FLOADER_NAME:"", //域名下静态资源目录 可以为空
   BUILD_PATH:"js/app", //webPack 编译路径
-
+  PAY_DOMAIN:payDomain,//支付跳转页路径
   SERIALTYPE:{
     1:"全车系", 2:"指定车系"
   },
